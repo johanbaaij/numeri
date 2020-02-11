0460c99c22214f230bd2adcc2785bead81175005
Johan Baaij <johanbaaij@gmail.com>
2020-02-11 11:16:51 +1100

    refactor: symbolDictionary as Array for order
    Shorter names for numeral properties and variables.

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,59 @@
-const symbolsDictionary: { [key: number]: string } = {
-  1000: 'M',
-  500: 'D',
-  100: 'C',
-  50: 'L',
-  10: 'X',
-  5: 'V',
-  1: 'I',
-};
-
 class Numeri {
-  arabicNumeral: number;
-  numberLeftToConvert: number;
-  romanNumeral: string;
+  integer: number;
+  roman: string;
+  integerRemains: number;
 
-  constructor(arabicNumeral: number | keyof typeof symbolsDictionary) {
-    this.arabicNumeral = arabicNumeral;
-    this.numberLeftToConvert = parseInt(`${arabicNumeral}`);
-    this.romanNumeral = '';
-    if (this.arabicNumeral > 3999) {
+  constructor(integer: number) {
+    this.integer = integer;
+    this.integerRemains = integer;
+    this.roman = '';
+
+    if (this.integer > 3999) {
       throw new Error('can only convert up to 3999');
     }
   }
 
   public convertToRoman() {
-    if (this.arabicNumeral in symbolsDictionary)
-      return symbolsDictionary[this.arabicNumeral];
+    const findInDictionary = this.findIntegerInDictionary(this.integer);
+    if (findInDictionary) return findInDictionary;
 
     this.symbolModulos();
-    return this.romanNumeral;
+    return this.roman;
   }
 
-  private symbolModulos() {
-    [1000, 500, 100, 50, 10, 5, 1].forEach(value => {
+  findIntegerInDictionary(integer: number): string | boolean {
+    const pair = symbolsDictionary.find(pair => pair.integer === integer);
+    return pair?.roman || false;
+  }
+
+  symbolModulos() {
+    symbolsDictionary.forEach(pair => {
       const numberOfMultiples =
-        (this.numberLeftToConvert - (this.numberLeftToConvert % value)) / value;
-      this.numberLeftToConvert =
-        this.numberLeftToConvert - numberOfMultiples * value;
-      this.appendToOutput(symbolsDictionary[value].repeat(numberOfMultiples));
+        (this.integerRemains - (this.integerRemains % pair.integer)) /
+        pair.integer;
+
+      this.integerRemains =
+        this.integerRemains - numberOfMultiples * pair.integer;
+      this.appendToOutput(pair.roman.repeat(numberOfMultiples));
     });
   }
 
-  private appendToOutput(string: string) {
-    this.romanNumeral = this.romanNumeral.concat(string);
+  appendToOutput(string: string) {
+    this.roman = this.roman.concat(string);
   }
 }
 
-export const toRoman = (arabicNumeral: number) => {
-  const numeri = new Numeri(arabicNumeral);
+export const toRoman = (integer: number) => {
+  const numeri = new Numeri(integer);
   return numeri.convertToRoman();
 };
+
+const symbolsDictionary: Array<{ integer: number; roman: string }> = [
+  { integer: 1000, roman: 'M' },
+  { integer: 500, roman: 'D' },
+  { integer: 100, roman: 'C' },
+  { integer: 50, roman: 'L' },
+  { integer: 10, roman: 'X' },
+  { integer: 5, roman: 'V' },
+  { integer: 1, roman: 'I' },
+];
